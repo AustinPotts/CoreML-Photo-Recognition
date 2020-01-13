ae57d3b6353f7b4adc0e070a2fd222ba7e0d0ca5
AustinPotts <pottsevolvements@gmail.com>
2020-01-13 13:03:09 -0500

    Configured Storyboard, Outlets & Actions

--- Photo-Recognition-xcode/Photo-Recognition-xcode/ViewController.swift
+++ Photo-Recognition-xcode/Photo-Recognition-xcode/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imager: UIImageView!
+    @IBOutlet weak var photoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func openLibrary(_ sender: Any) {
+        let picker = UIImagePickerController()
+           picker.allowsEditing = false
+           picker.delegate = self
+           picker.sourceType = .photoLibrary
+           present(picker, animated: true)
+    }
+    
+    @IBAction func camera(_ sender: Any) {
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+              return
+          }
+          
+          let cameraPicker = UIImagePickerController()
+          cameraPicker.delegate = self
+          cameraPicker.sourceType = .camera
+          cameraPicker.allowsEditing = false
+          
+          present(cameraPicker, animated: true)
+    }
+    
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
